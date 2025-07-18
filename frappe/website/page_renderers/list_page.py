cf31e44e4c884b00d91606091f7b40697d1c9129
Development for People <47140294+developmentforpeople@users.noreply.github.com>
2025-07-18 11:35:33 +0000

    DFP FIX issue for doctype list pages without has_web_view

--- frappe/website/page_renderers/list_page.py
+++ frappe/website/page_renderers/list_page.py
@@ -4,7 +4,8 @@ from frappe.website.page_renderers.template_page import TemplatePage
 
 class ListPage(TemplatePage):
 	def can_render(self):
-		return frappe.db.exists("DocType", self.path, True)
+		# DFP added check if has_web_view if not not allowed to be rendered; problem: dt as dict makes query not cacheable
+		return frappe.db.exists(dt={"doctype": "DocType", "name": self.path, "has_web_view": 1})
 
 	def render(self):
 		frappe.local.form_dict.doctype = self.path
